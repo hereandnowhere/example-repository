397405179d9552814435609b3892f07f597ff9e4
Jacob Vine <jvine1235@gmail.com>
2022-01-22 11:14:23 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+h2.style.color = "#f0ffff";
+h2.style.textAlign = "center";
+
+document.querySelector("body").appendChild(h2);
